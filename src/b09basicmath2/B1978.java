bc3b15bb2b8a4ebd3e30d107f2ed7367e46d8815
khj1 <dhepd8849@gmail.com>
2021-05-15 19:47:19 +0900

    2021-05-15

---
+++ src/b09basicmath2/B1978.java
@@ -0,0 +1,31 @@
+package b09basicmath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class B1978 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 0;
+		for(int i=0; i<t; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			int m = 0;
+			for(int j=1; j<=n; j++) {
+				if(m>2)
+					break;
+				if(n%j==0)
+					m++;
+			}
+			if(m==2)
+				cnt++;
+		}
+		System.out.println(cnt);
+	}
+
+}
